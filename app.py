19245dbc9a92e6dc256a85475914fabe984ef477
Manasa B M <bm.maanasa@gmail.com>
2026-09-17 15:55:06 +0530

    added flower name

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app = FastAPI()
 model_path = os.path.join(os.path.dirname(__file__), "model.pkl")
 model = joblib.load(model_path)
 
+# 0,1,2 ge name map
+class_names = {0: "Setosa", 1: "Versicolor", 2: "Virginica"}
+
 @app.get("/")
 def home():
     return {"message": "MLOps Iris API is Running"}
@@ -17,6 +20,8 @@ def predict(data: dict):
     try:
         features = np.array(data["features"]).reshape(1, -1)
         prediction = model.predict(features)
-        return {"prediction": int(prediction[0])}
+        pred_id = int(prediction[0])
+        pred_name = class_names[pred_id]
+        return {"prediction_id": pred_id, "prediction_name": pred_name}
     except Exception as e:
         return {"error": str(e)}
